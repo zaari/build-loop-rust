b31f827c750564fcb81c56a023e949ac37707fa6
Timo Saarinen <zaari@mailbox.org>
2023-01-11 20:03:39 +0200

    Added cargo-watch as an option to inotifywait

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 
 # build-loop-rust
 
-This is a small script which helps Rust development when an IDE is not used.
-Its behaviour is similar to `cargo watch` command.
-The script is run on the same directory where Cargo.toml file is located. 
+Cyclic build script which builds when a file changes. Helpful when editing Rust 
+source code without a proper IDE or language server. This script uses cargo-watch if 
+it is available or Linux inotify if not.
 
 # Features
 
@@ -18,8 +18,9 @@ The script is run on the same directory where Cargo.toml file is located.
 You have to add the location of the script to PATH environment variable or
 save (or link) the script to a location on the PATH.
 
-It's expected that [cargo-audit] and [cargo-outdated] extensions are installed.
+It's recommended that [cargo-watch], [cargo-audit] and [cargo-outdated] extensions are installed.
 
+[cargo-watch]: https://github.com/watchexec/cargo-watch
 [cargo-audit]: https://github.com/RustSec/cargo-audit
 [cargo-outdated]: https://github.com/kbknapp/cargo-outdated
 
--- build-loop-rust.sh
+++ build-loop-rust.sh
@@ -2,12 +2,13 @@
 
 #
 # Cyclic build script which builds when a file changes. Helpful when editing Rust 
-# source code without an IDE or language server. This script is similar to cargo-watch extension.
+# source code without an IDE or language server. This script uses cargo-watch if 
+# it is available or Linux inotify if not.
 #
 # Dependencies: 
-#   inotify-tools (Linux-only)
-#   cargo-audit
-#   cargo-outdated
+#   cargo-watch OR inotify-tools (Linux-only)
+#   cargo-audit (optional)
+#   cargo-outdated (optional)
 #
 
 COMMAND=${1:-test}
@@ -23,15 +24,23 @@ cargo update && echo ""
 cargo help outdated &>/dev/null && cargo outdated -R && sleep 1s && echo ""
 cargo help audit &>/dev/null && cargo audit && sleep 1s && echo ""
 
-# The initial build
-shift 2>/dev/null
-echo "Running cargo with subcommand '$COMMAND' $*"
-cargo $COMMAND $*
-
-# Build on demand
-while inotifywait -e modify --exclude target -r . &>/dev/null ; do
-    sync
-    clear
+# Use cargo-watch if it is installed
+if cargo watch --version &>/dev/null ; then
+    # The initial build
     cargo $COMMAND $*
-done
+    
+    # Build on demand
+    cargo watch --postpone -cx $COMMAND $*
+else
+    # The initial build
+    shift 2>/dev/null
+    echo "Running cargo with subcommand '$COMMAND' $*"
+    cargo $COMMAND $*
 
+    # Build on demand
+    while inotifywait -e modify --exclude target -r . &>/dev/null ; do
+        sync
+        clear
+        cargo $COMMAND $*
+    done
+fi
